e34d0cbc857201b851eeb3b27dfbfa99f2a8a0e1
Mario Sanz <mariogsanz@localhost.localdomain>
2022-05-03 11:46:50 +0200

    Update

--- rol.sh
+++ rol.sh
@@ -20,13 +20,14 @@ function dashboard(){
 }
 
 # Declare variables
-let total_steps=50+$(($RANDOM%51))
-let total_life=5+$(($RANDOM%16))
+let total_steps=50+$(($RANDOM%51))	# Total of steps remaining
+let total_life=5+$(($RANDOM%16))	# Total of life remaining
 
-let move_dice
+let move_dice	# Array with movement dices
 
-dashboard
+dashboard	# Print dashboard
 
+# Throw dices to move
 read -s -n 1 -p "Press any key to throw a dice..."
 echo
 
@@ -35,6 +36,6 @@ for i in $(seq 1 2); do
 done
 
 total_steps=$(($total_steps-(${move_dice[1]}+${move_dice[2]})))
-echo 
 
+echo 
 dashboard
