7ba0bf3ee63c456b98991f72db23b8fcab4387e8
Mario Sanz <mariogsanz@localhost.localdomain>
2022-05-09 14:18:08 +0200

    Update

--- rol.sh
+++ rol.sh
@@ -9,4 +9,38 @@
 #                                                               #
 #################################################################
 
+##### VARIABLES #####
+movement=$(($RANDOM%51+50))
+life=0
 
+##### MOVEMENT #####
+move () {
+  for i in $(seq 1 $1)
+  do
+    let life=$(($life+5)) 
+  done
+  
+}
+
+##### SELECT YOUR PLAYER #####
+player () {
+  echo " __"
+  echo "(_  _ | _  __|_    _    ._ ._ | _.   _ ._"
+  echo "__)(/_|(/_(_ |_ \/(_)|_||  |_)|(_|\/(/_|"
+  echo "                /          |      /"
+  echo
+  echo "1) Barbarian"
+  echo "2) Assassin"
+  echo "3) Pariah (!!! Hardcore Mode !!!)"
+  
+  read -s -n 1 player
+  
+  case $player in
+    1)
+      move 2
+    ;;
+  esac
+  
+}
+
+player
