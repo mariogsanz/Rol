d66a2411e0bfdb9c260a69bc255b9c2e18bdbae5
Mario Sanz <mariogsanz@localhost.localdomain>
2022-05-09 12:11:14 +0200

    New Game

--- rol.sh
+++ rol.sh
@@ -1,41 +1,12 @@
 #!/bin/bash
 
-##
-#
-# ROL
-#
-##
+#################################################################
+#        ____       _    ____                                   #
+#       |  _ \ ___ | |  / ___| __ _ _ __ ___   ___              #
+#       | |_) / _ \| | | |  _ / _` | '_ ` _ \ / _ \             #
+#       |  _ < (_) | | | |_| | (_| | | | | | |  __/             #
+#       |_| \_\___/|_|  \____|\__,_|_| |_| |_|\___|             #
+#                                                               #
+#################################################################
 
-clear
 
-# Functions
-
-function dashboard(){
-	echo "#################################################"
-	echo "#	Steps		|	Life		#"
-	echo "#-----------------------------------------------#"
-	echo "#	$total_steps		|	$total_life		#"
-	echo "#################################################"
-	echo
-}
-
-# Declare variables
-let total_steps=50+$(($RANDOM%51))	# Total of steps remaining
-let total_life=5+$(($RANDOM%16))	# Total of life remaining
-
-let move_dice	# Array with movement dices
-
-dashboard	# Print dashboard
-
-# Throw dices to move
-read -s -n 1 -p "Press any key to throw a dice..."
-echo
-
-for i in $(seq 1 2); do
-	let move_dice[$i]=$(($RANDOM%6+1))
-done
-
-total_steps=$(($total_steps-(${move_dice[1]}+${move_dice[2]})))
-
-echo 
-dashboard
