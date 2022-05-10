9e5bfa38f4628ec8e1123b0f0720c1a9cca70bbf
mariogsanz <mariogsanz@hotmail.com>
2022-05-10 14:22:53 +0200

    update

--- rol.sh
+++ rol.sh
@@ -13,13 +13,23 @@
 movement=$(($RANDOM%51+50))
 life=0
 
+##### SET LIFE #####
+set_life () {
+  for i in $(seq 1 $1); do
+    life=$(($life+$RANDOM%6+1))
+  done
+}
+
 ##### MOVEMENT #####
 move () {
-  for i in $(seq 1 $1)
+  local move=0
+
+  for i in $(seq 1 2)
   do
-    let life=$(($life+5)) 
+    move=$(($RANDOM%6+1+$move))
   done
   
+  movement=$(($movement-$move))
 }
 
 ##### SELECT YOUR PLAYER #####
@@ -32,15 +42,50 @@ player () {
   echo "1) Barbarian"
   echo "2) Assassin"
   echo "3) Pariah (!!! Hardcore Mode !!!)"
-  
+  echo "0) ----- EXIT -----"
+
   read -s -n 1 player
   
   case $player in
     1)
-      move 2
+      echo "You select the BARBARIAN"
+      set_life 4
+    ;;
+
+    2)
+      echo "You select the ASSASSIN"
+      set_life 2
+    ;;
+
+    3)
+      echo "You select the PARIAH. Get ready to SUFFER!!!"
+      set_life 1
+    ;;
+
+    0)
+      exit 0
+    ;;
+
+    *)
+      
     ;;
   esac
   
 }
 
+##### DASHBOARD #####
+dashboard () {
+  echo "######################################################"
+  echo "#                          |                         #"
+  echo "#           Steps          |           Life          #"
+  echo "#             $movement           |            $life            #"
+  echo "#                          |                         #"
+  echo "######################################################"
+}
+
 player
+
+while true; do
+  dashboard
+  sleep 1
+done
